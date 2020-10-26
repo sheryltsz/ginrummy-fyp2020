4471fed4e6cb472a823a13405d733e5ff7db8e0f
sheryltsz <70138510+sheryltsz@users.noreply.github.com>
2020-10-26 17:56:53 +0800

    Initialise

---
+++ Assets/Script/GameManagerScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public Sprite[] CardFaces;
+
+    public GameObject CardPrefab;
+
+    //S for Spade, C for Clubs, D for Diamonds, H for Hearts
+    public static string[] suits = { "C", "D", "H", "S" };
+
+    public static string[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" }; 
+
+    public List<string> deck;
+
+    public List<string> shuffledDeck;
+
+    public GameObject CardContainer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayCards();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void PlayCards() {
+
+        deck = GenerateDeck();
+        shuffledDeck = deck;
+        shuffledDeck = Shuffle(shuffledDeck);
+        CardDeal();
+        //foreach (string card in deck)
+        //{
+        //    print(card);
+        //}
+    }
+
+    //when the game starts, create card that is in order: C1 to C13, D1 to D13, H1 to H13, S1 to S13
+    public static List<string> GenerateDeck() {
+
+        List<string> newDeck = new List<string>();
+
+        //this nested loop creates a card deck of 52 cards 
+        foreach (string s in suits)
+        {
+            foreach (string v in values)
+            {
+                newDeck.Add(s + v);
+            }
+        }
+
+        return newDeck;
+    }
+
+
+    //shuffle the deck created to form a deck of card w random sequeunce. 
+    public static List<T> Shuffle<T>(List<T> list) {
+
+        System.Random random = new System.Random();
+
+        int n = list.Count;
+
+        while (n > 1) {
+
+            int k = random.Next(n);
+            n--;
+
+            //swap positions of index k and n, k at random
+            T temp = list[k];
+            list[k] = list[n];
+            list[n] = temp;
+
+        }
+        return list;
+
+    }
+
+    //spawn deck cards
+    public void CardDeal() {
+
+        foreach (string card in deck)
+        {
+            //Instantiate CardPrefabs in CardContainer
+            GameObject newCard = Instantiate(CardPrefab, CardContainer.transform);
+            //name is C1 to C13, D1 to D13, H1 to H13, S1 to S13 (shuffled)
+            newCard.name = card;
+        }
+    }
+
+}
---
+++ Assets/Script/UpdateSprite.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateSprite : MonoBehaviour
+{
+
+    public Sprite cardFace;
+    public Sprite cardBack;
+
+    private SpriteRenderer spriteRenderer;
+    public bool onCardFace = false;
+    GameManagerScript gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameManagerScript>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+
+        List<string> deck = gm.deck;
+
+        int i = 0;
+
+        foreach (string card in deck)
+        {
+            if (this.name == card) {
+
+                cardFace = gm.CardFaces[i];
+                spriteRenderer.sprite = cardFace;
+                break;
+            }
+            i++;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onCardFace)
+        {
+            spriteRenderer.sprite = cardFace;
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+        }
+        
+
+    }
+}
